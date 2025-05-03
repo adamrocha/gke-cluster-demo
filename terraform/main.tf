9001db70d5e7152372ee1f62e8928fbb419fff0d
Adam Rocha <adamrocha13@gmail.com>
2025-05-03 14:47:59 -0700

    feat: add IAM roles for GKE service account and update project ID in variables.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,44 +23,100 @@ resource "google_project_service" "cloud-monitoring-api" {
 }
 */
 
-resource "google_container_cluster" "gke-cluster" {
-  depends_on = [
-    google_project_service.compute-api,
-    google_project_service.container-api
+# IAM roles for the service account
+locals {
+  sa_roles = [
+    "roles/container.nodeServiceAccount",
+    "roles/logging.logWriter",
+    "roles/monitoring.metricWriter",
+    "roles/compute.networkUser",
+    "roles/editor"
   ]
-  name                     = "gke-cluster"
-  network                  = google_compute_network.vpc_network.name
-  subnetwork               = google_compute_subnetwork.subnet.name
-  remove_default_node_pool = true
-  initial_node_count       = 1
-  deletion_protection      = false
-  ip_allocation_policy {}
 }
 
-resource "google_container_node_pool" "gke_preemptible_nodes" {
-  depends_on = [google_container_cluster.gke-cluster]
-  name       = "gke-node-pool"
-  cluster    = google_container_cluster.gke-cluster.name
-  node_count = 1
-
-  node_config {
-    preemptible  = true
-    machine_type = "e2-micro"
-    //service_account = google_service_account.default[0].email
-    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
-  }
+resource "google_project_iam_member" "gke-node-pool-roles" {
+  for_each = toset(local.sa_roles)
+  project  = var.project_id
+  role     = each.key
+  member   = "serviceAccount:${google_service_account.gke-service-account.email}"
 }
 
-/*
 // Optional: Service account (conditionally created)
-resource "google_service_account" "default" {
-  depends_on   = []
-  count        = var.create_service_account ? 1 : 0
+// Using a dedicated SA for GKE nodes is a security best practice.
+resource "google_service_account" "gke-service-account" {
   account_id   = "gke-service-account"
   display_name = "GKE Service Account"
   description  = "Service account for GKE"
+
+  lifecycle {
+    prevent_destroy = false
+  }
 }
-*/
+
+resource "google_container_cluster" "gke-cluster" {
+  depends_on = [
+    google_project_service.compute-api,
+    google_project_service.container-api,
+    google_service_account.gke-service-account,
+    google_project_iam_member.gke-node-pool-roles
+  ]
+  name                = "node-pool-cluster"
+  network             = google_compute_network.vpc_network.name
+  subnetwork          = google_compute_subnetwork.subnet.name
+  initial_node_count  = 1
+  deletion_protection = false
+  //remove_default_node_pool = false
+  ip_allocation_policy {}
+}
+
+resource "google_container_node_pool" "gke-node-pool" {
+  depends_on = [
+    google_container_cluster.gke-cluster,
+    google_project_service.compute-api,
+    google_project_service.container-api,
+    google_compute_network.vpc_network,
+    google_compute_subnetwork.subnet,
+    google_service_account.gke-service-account
+  ]
+  //name               = "gke-node-pool"
+  cluster = google_container_cluster.gke-cluster.name
+  //initial_node_count = 3
+
+  node_config {
+    service_account = google_service_account.gke-service-account.email
+    preemptible     = true
+    machine_type    = "e2-micro"
+    # Using a dedicated service account is recommended over the default compute SA
+    # Using a custom service account for GKE nodes is a security best practice
+    # The default compute service account has broad permissions
+    # and should be avoided for GKE nodes.
+    # The service account should have the necessary IAM roles for GKE
+    # and any other services the nodes need to access.
+    # For example, if the nodes need to access Cloud Storage, you can add:
+    # roles/storage.objectViewer
+    # roles/storage.objectAdmin
+    # roles/storage.admin
+    # roles/logging.logWriter
+    # roles/monitoring.metricWriter
+    # roles/monitoring.viewer
+    # roles/monitoring.admin
+    # roles/compute.viewer
+    # roles/compute.networkViewer
+    # roles/compute.securityAdmin
+    # roles/compute.instanceAdmin
+    # roles/compute.networkAdmin
+    # roles/compute.autoscaler
+    # roles/compute.networkUser
+    # roles/compute.storageAdmin
+    # roles/compute.viewer
+    # roles/compute.networkAdmin
+    # roles/compute.securityAdmin
+    # roles/compute.instanceAdmin
+    # roles/compute.networkViewer
+    # Consider using more restrictive scopes based on workload needs for production
+    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+}
 
 /*
 resource "google_container_cluster" "autopilot" {
