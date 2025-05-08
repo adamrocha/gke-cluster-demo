1b9ee2a4a1151e65a365d1931f9fdb50b2c3776f
Adam Rocha <adamrocha13@gmail.com>
2025-05-07 19:44:32 -0700

    feat: update GCP project services and IAM roles for GKE and Ansible integration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,8 @@
-resource "google_project_service" "gke-services" {
+resource "google_project_service" "api-services" {
   for_each = toset([
     "compute.googleapis.com",
-    "container.googleapis.com"
+    "container.googleapis.com",
+    "secretmanager.googleapis.com"
   ])
   project                    = var.project_id
   service                    = each.key
@@ -27,16 +28,73 @@ resource "google_project_iam_member" "gke-sa-roles" {
 resource "google_service_account" "gke-service-account" {
   account_id   = "gke-service-account"
   display_name = "GKE Service Account"
-  description  = "Service account for GKE"
+  description  = "GKE Service Account"
 
   lifecycle {
     prevent_destroy = false
   }
 }
 
+resource "google_project_iam_member" "ansible-sa-roles" {
+  for_each = toset([
+    "roles/container.nodeServiceAccount",
+    "roles/iam.serviceAccountViewer",
+    "roles/compute.viewer",
+    "roles/editor"
+  ])
+  project = var.project_id
+  role    = each.key
+  member  = "serviceAccount:${google_service_account.ansible-service-account.email}"
+}
+
+resource "google_service_account" "ansible-service-account" {
+  account_id   = "ansible-service-account"
+  display_name = "Ansible Service Account"
+  description  = "Ansible Service Account"
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "google_service_account_key" "ansible-inventory-key" {
+  service_account_id = google_service_account.ansible-service-account.name
+  private_key_type   = "TYPE_GOOGLE_CREDENTIALS_FILE"
+  depends_on = [
+    google_project_service.api-services,
+    google_service_account.ansible-service-account,
+    google_project_iam_member.ansible-sa-roles
+  ]
+
+  provisioner "local-exec" {
+    command = "echo '${google_service_account_key.ansible-inventory-key.private_key}' > /opt/keys/ansible-inventory-key.json"
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "google_secret_manager_secret" "ansible-key-secret" {
+  secret_id = "ansible-inventory-key"
+  replication {
+    user_managed {
+      replicas {
+        location = "us-central1"
+      }
+    }
+  }
+}
+
+resource "google_secret_manager_secret_version" "ansible-key-secret-version" {
+  secret = google_secret_manager_secret.ansible-key-secret.id
+  //secret_data = google_service_account_key.ansible-inventory-key.private_key
+  secret_data = file("/opt/keys/ansible-inventory-key.json")
+}
+
 resource "google_container_cluster" "gke-cluster" {
   depends_on = [
-    google_project_service.gke-services,
+    google_project_service.api-services,
     google_service_account.gke-service-account,
     google_project_iam_member.gke-sa-roles
   ]
@@ -59,7 +117,7 @@ resource "google_container_cluster" "gke-cluster" {
 
 resource "google_container_node_pool" "gke-pool" {
   depends_on = [
-    google_project_service.gke-services,
+    google_project_service.api-services,
     google_service_account.gke-service-account
   ]
   name               = "gke-pool"
