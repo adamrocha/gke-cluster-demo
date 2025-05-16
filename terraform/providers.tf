8234b8af7cbbf8cf7240e4eeb6d2bbc071fa1aa3
Adam Rocha <adamrocha13@gmail.com>
2025-05-15 17:33:54 -0700

    feat: add Kubernetes provider configuration to Terraform

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,3 +9,9 @@ provider "google-beta" {
   region  = var.region
   zone    = var.zone
 }
+
+provider "kubernetes" {
+  host                   = "https://${google_container_cluster.gke_cluster.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+}
