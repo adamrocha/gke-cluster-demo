f56bd5d90a5a482562ea4e547b19a9b4162b7500
Adam Rocha <adamrocha13@gmail.com>
2025-05-26 21:20:12 -0700

    fix: comment out gke_service_account output in Terraform configuration

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -19,11 +19,11 @@ output "gke_cluster_endpoint" {
   value       = google_container_cluster.gke_cluster.endpoint
   description = "hello-world endpoint"
 }
-
+/*
 output "gke_service_account" {
   value = google_container_cluster.gke_cluster.node_config[0].service_account
 }
-
+*/
 output "load_balancer_ip" {
   value       = kubernetes_service.hello_world_service.status[0].load_balancer[0].ingress[0].ip
   description = "External IP of the hello-world service"
