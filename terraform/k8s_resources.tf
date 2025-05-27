727ea182a7f1f5d844da8325274027c0057f01b7
Adam Rocha <adamrocha13@gmail.com>
2025-05-27 11:38:11 -0700

    style: format code for consistency in k8s_resources.tf

--- terraform/k8s_resources.tf
+++ terraform/k8s_resources.tf
@@ -55,8 +55,8 @@ resource "kubernetes_deployment" "hello_world" {
         max_unavailable = "25%"
       }
     }
-    revision_history_limit = 10
-    min_ready_seconds      = 5
+    revision_history_limit    = 10
+    min_ready_seconds         = 5
     progress_deadline_seconds = 120
 
     selector {
@@ -78,17 +78,17 @@ resource "kubernetes_deployment" "hello_world" {
         }
 
         container {
-          name              = "hello-world"
+          name = "hello-world"
           //image             = "gcr.io/gke-cluster-458701/hello-world:1.0.0@sha256:1eb501d45bf85b69c6e235c70db971872d82d956a8cd0ab875002894270ff65b"
           image             = "gcr.io/gke-cluster-458701/hello-world:1.1.3@sha256:61642948bd3df265018c1fa3b256ac9ab2ae1c7f808611e534b18391137616d7"
           image_pull_policy = "Always"
 
           security_context {
             allow_privilege_escalation = true
-            run_as_non_root = false
-            run_as_user     = 0
-            run_as_group    = 0
-            read_only_root_filesystem = false
+            run_as_non_root            = false
+            run_as_user                = 0
+            run_as_group               = 0
+            read_only_root_filesystem  = false
           }
 
           port {
