e609960943a2c8c2028cf93be257ac44fb1d12e9
Adam Rocha <adamrocha13@gmail.com>
2025-06-03 22:51:48 -0700

    fix: remove duplicate descriptions in logging.tf

--- terraform/logging.tf
+++ terraform/logging.tf
@@ -1,11 +1,11 @@
 resource "google_logging_project_bucket_config" "log_analytics_bucket" {
+  description      = "Bucket for storing logs and analytics data"
   depends_on       = [google_project_service.api_services]
   project          = var.project_id
   bucket_id        = "_Default"
   location         = "global"
   enable_analytics = true
   retention_days   = 1
-  description      = "Bucket for storing logs and analytics data"
 
   lifecycle {
     prevent_destroy = false
@@ -13,6 +13,7 @@ resource "google_logging_project_bucket_config" "log_analytics_bucket" {
 }
 
 resource "google_logging_project_sink" "route_to_log_analytics" {
+  description            = "Route logs to the analytics bucket"
   name                   = "vpc-logs-to-analytics"
   destination            = "logging.googleapis.com/projects/${var.project_id}/locations/global/buckets/_Default"
   filter                 = ""
