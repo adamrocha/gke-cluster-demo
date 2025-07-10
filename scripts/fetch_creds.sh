52f57038bdf69806681e47c594c4c9561bf9ae3f
Adam Rocha <adamrocha13@gmail.com>
2025-07-10 04:22:00 -0700

    refactor: use variables for project ID and region in fetch_creds.sh

--- scripts/fetch_creds.sh
+++ scripts/fetch_creds.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # This script fetches credentials for a Google Cloud project and sets up Docker authentication for Google Container Registry (GCR).
 
+PROJECT_ID="gke-cluster-458701"
+REGION="us-central1"
+
 gcloud container clusters get-credentials demo-cluster \
-  --region=us-central1 \
-  --project=gke-cluster-458701
+  --region "$REGION" \
+  --project "$PROJECT_ID"
