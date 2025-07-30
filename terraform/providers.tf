bbf0c1986cda4ff191ad4d2edf66844ab2b0fca3
Adam Rocha <adamrocha13@gmail.com>
2025-07-30 16:59:28 -0700

    fix: add missing configuration for helm provider in providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -18,4 +18,11 @@ provider "kubernetes" {
 }
 
 provider "helm" {
+  kubernetes = {
+    host                   = "https://${google_container_cluster.gke_cluster.endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(google_container_cluster.gke_cluster.master_auth[0].cluster_ca_certificate)
+    config_path            = var.kubeconfig_path
+  }
 }
+
