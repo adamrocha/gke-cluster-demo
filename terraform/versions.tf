9e2eb8db427e0c88940ffab8bc41e51ee8e61cc8
Adam Rocha <adamrocha13@gmail.com>
2025-08-15 00:41:43 -0700

    refactor: remove required_version constraint from terraform block

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,10 +1,16 @@
 terraform {
-  required_version = "> 1.5"
-
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.20"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12"
     }
   }
 }
