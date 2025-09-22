5856796f933941e694cfa7e12cd73341f5388c2e
Adam Rocha <adamrocha13@gmail.com>
2025-09-22 01:49:32 +0000

    fix: comment out helm_release resource for Prometheus in monitoring.tf

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -1,15 +1,15 @@
-resource "helm_release" "prometheus" {
-  depends_on       = [google_container_node_pool.gke_pool]
-  name             = "prometheus"
-  repository       = "https://prometheus-community.github.io/helm-charts"
-  chart            = "kube-prometheus-stack"
-  namespace        = var.monitoring_ns
-  create_namespace = true
-  timeout          = 600
-  skip_crds        = false
-  wait             = true
-  version          = "76.4.0"
-}
+# resource "helm_release" "prometheus" {
+#   depends_on       = [google_container_node_pool.gke_pool]
+#   name             = "prometheus"
+#   repository       = "https://prometheus-community.github.io/helm-charts"
+#   chart            = "kube-prometheus-stack"
+#   namespace        = var.monitoring_ns
+#   create_namespace = true
+#   timeout          = 600
+#   skip_crds        = false
+#   wait             = true
+#   version          = "76.4.0"
+# }
 #   set {
 #     name  = "grafana.enabled"
 #     value = "true"
