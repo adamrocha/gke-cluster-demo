141659f608428fef01cdd266abab7d0927bcd877
Adam Rocha <adamrocha13@gmail.com>
2025-12-03 21:08:00 -0800

    Remove incorrect IAM roles for artifact registry in GKE service account configuration

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -30,8 +30,6 @@ resource "google_project_iam_member" "gke_sa_roles" {
   for_each = toset([
     "roles/container.defaultNodeServiceAccount",
     "roles/artifactregistry.admin",
-    "roles/artifactory.wriiter",
-    "Roles/artifactory.reader",
     "roles/storage.objectViewer",
     "roles/logging.logWriter",
     "roles/monitoring.metricWriter"
