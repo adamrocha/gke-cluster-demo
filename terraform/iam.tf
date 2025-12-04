49df56baa4b91c2af6cb6ab405307cb9aff52b41
Adam Rocha <adamrocha13@gmail.com>
2025-12-03 23:48:56 -0800

    Update IAM roles in iam.tf: remove unnecessary roles and correct role names for GKE service account

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -23,19 +23,13 @@ resource "google_service_account" "gke_service_account" {
 
 resource "google_project_iam_member" "gke_sa_roles" {
   for_each = toset([
-    "roles/artifactory.writer",
-    "roles/artifactory.reader",
-    "roles/container.clusterAdmin",
-    "roles/container.admin",
-    "roles/compute.networkAdmin",
-    "roles/logging.logWriter",
-    "roles/storage.admin",
-    "roles/storage.objectAdmin",
-    "roles/storage.objectCreator",
-    "roles/storage.objectViewer",
+    "roles/container.defaultNodeServiceAccount",
     "roles/artifactregistry.admin",
-    "roles/monitoring.metricWriter",
-    "roles/container.defaultNodeServiceAccount"
+    "roles/artifactory.wriiter",
+    "Roles/artifactory.reader",
+    "roles/storage.objectViewer",
+    "roles/logging.logWriter",
+    "roles/monitoring.metricWriter"
   ])
   project = var.project_id
   role    = each.key
